d05a7c4b9e39f4229320b91f26861719dfdda566
alex-sumy-ua <plachkovskyy@gmail.com>
2024-09-17 15:03:41 +0100

    feat: Task 6: Creating A Retrofit Instance - completed

---
+++ app/src/main/java/com/northcoders/jv_fe_http_requests_retrofit/repository/ApiService.java
@@ -0,0 +1,15 @@
+package com.northcoders.jv_fe_http_requests_retrofit.repository;
+
+import com.northcoders.jv_fe_http_requests_retrofit.model.Fruit;
+
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface ApiService {
+
+    @GET
+    Call<List<Fruit>> getAllFruits();
+    
+}
---
+++ app/src/main/java/com/northcoders/jv_fe_http_requests_retrofit/repository/RetrofitInstance.java
@@ -0,0 +1,34 @@
+package com.northcoders.jv_fe_http_requests_retrofit.repository;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import okhttp3.logging.HttpLoggingInterceptor;
+
+public class RetrofitInstance {
+
+    private static Retrofit retrofit = null;
+
+    private final static String BASE_URL = "http://fruityvice.com/api/fruit/";
+
+
+
+
+    public static ApiService getService() {
+
+        HttpLoggingInterceptor interceptor = new HttpLoggingInterceptor();
+        interceptor.setLevel(HttpLoggingInterceptor.Level.BODY);
+        OkHttpClient client = new OkHttpClient.Builder().addInterceptor(interceptor).build();
+
+        if (retrofit == null) {
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .client(client)
+                    .build();
+        }
+
+        return retrofit.create(ApiService.class);
+
+    }
+
+
+}
